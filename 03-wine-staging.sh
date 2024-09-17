ef95b8eb7c3a6577db0297bb2115bc72cb4959a9
Kolefell <57557101+koleffel@users.noreply.github.com>
2024-09-17 05:23:42 +0300

    Update 03-wine-staging.sh
    Updating the wine-staging install for the Linux Mint 22

--- 03-wine-staging.sh
+++ 03-wine-staging.sh
@@ -11,7 +11,7 @@ sudo mkdir -pm755 /etc/apt/keyrings
 
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 
 sudo apt update
 
