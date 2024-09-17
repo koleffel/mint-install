e042cee7fbcf47d2395bc94b046928972c45e0f8
Kolefell <57557101+koleffel@users.noreply.github.com>
2024-09-17 05:20:54 +0300

    separate Flatpaks and apt installs
    Removing the Flatpaks and trying to separate them from the apt installs .

--- 02-install.sh
+++ 02-install.sh
@@ -5,52 +5,6 @@
 ########## e-mail : kolefell@proton.me ####################
 ###########################################################
 
-
-# KDENLIVE
-flatpak install flathub org.kde.kdenlive -y
-
-#obs-studio
-flatpak install flathub com.obsproject.Studio -y
-
-#brave browser
-flatpak install flathub com.brave.Browser -y
-
-#qbittorrent
-flatpak install flathub org.qbittorrent.qBittorrent -y
-
-#onlyoffice
-flatpak install flathub org.onlyoffice.desktopeditors -y
-
-#gimp
-flatpak install flathub org.gimp.GIMP -y
-
-#spotify
-flatpak install flathub com.spotify.Client -y
-
-#telegram
-flatpak install flathub org.telegram.desktop -y
-
-#viber
-#flatpak install flathub com.viber.Viber -y
-
-#sublime
-#flatpak install flathub com.sublimetext.three -y
-
-#VSCode
-flatpak install flathub com.visualstudio.code
-
-#blender
-flatpak install flathub org.blender.Blender -y 
-
-#handbrake
-flatpak install flathub fr.handbrake.ghb -y
-
-### Install FLATSEAL , for Flatpak permissions 
-
-flatpak install flathub com.github.tchx84.Flatseal -y
-
-# Installing from APT
-
 #git
 sudo apt install git -y
 
